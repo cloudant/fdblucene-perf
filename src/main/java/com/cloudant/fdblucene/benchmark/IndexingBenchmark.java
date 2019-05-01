3979b2e6545612bf9b4228f0c18a3be66203c5f6
Robert Newson <rnewson@apache.org>
2019-05-01 22:58:12 +0100

    use lucene-bench and wikipedia data

--- src/main/java/com/cloudant/fdblucene/benchmark/IndexingBenchmark.java
+++ src/main/java/com/cloudant/fdblucene/benchmark/IndexingBenchmark.java
@@ -4,20 +4,19 @@ import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Random;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.lucene.benchmark.byTask.feeds.ContentSource;
+import org.apache.lucene.benchmark.byTask.feeds.DocMaker;
+import org.apache.lucene.benchmark.byTask.feeds.EnwikiContentSource;
+import org.apache.lucene.benchmark.byTask.utils.Config;
 import org.apache.lucene.codecs.lucene80.Lucene80Codec;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.NIOFSDirectory;
-import org.apache.lucene.util.LineFileDocs;
-import org.apache.lucene.util.LuceneTestCase;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -25,12 +24,10 @@ import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Timeout;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -47,50 +44,42 @@ public class IndexingBenchmark {
     @BenchmarkMode(Mode.Throughput)
     @Fork(1)
     @State(Scope.Benchmark)
-    @Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
-    @Measurement(iterations = 3, time = 10, timeUnit = TimeUnit.MINUTES)
-    @Timeout(time = 30, timeUnit = TimeUnit.MINUTES)
+    @Warmup(iterations = 3, time = 10, timeUnit = TimeUnit.SECONDS)
+    @Measurement(iterations = 3, time = 2, timeUnit = TimeUnit.MINUTES)
     @OutputTimeUnit(TimeUnit.SECONDS)
     public static abstract class AbstractIndexingBenchmark {
         protected Database db;
         private Directory dir;
-        private Document doc;
         private IndexWriter writer;
-        private StringField idField;
-        private AtomicLong counter = new AtomicLong();
-        private Random random;
-        private LineFileDocs docs;
-
-        @Param({"true", "false"})
-        private boolean bigDocs;
+        private DocMaker docMaker;
 
         public abstract Directory getDirectory(final Path path) throws IOException;
 
         @Benchmark
         public long indexing() throws Exception {
-            if (bigDocs) {
-                doc = docs.nextDoc();
-            }
-            idField.setStringValue("doc-" + counter.incrementAndGet());
+            final Document doc = docMaker.makeDocument();
             return writer.addDocument(doc);
         }
 
         @Setup(Level.Iteration)
         public void setup() throws Exception {
-            final IndexWriterConfig config = indexWriterConfig();
+            final IndexWriterConfig indexWriterConfig = indexWriterConfig();
             dir = getDirectory(generateTestPath());
             cleanDirectory();
-            writer = new IndexWriter(dir, config);
-            random = new Random();
-            docs = new LineFileDocs(random, LuceneTestCase.DEFAULT_LINE_DOCS_FILE);
-            if (bigDocs) {
-                doc = docs.nextDoc();
-            } else {
-                doc = new Document();
-            }
-            idField = new StringField("_id", "", Store.YES);
-            doc.add(idField);
-            counter.set(0L);
+            writer = new IndexWriter(dir, indexWriterConfig);
+
+            Properties properties = new Properties();
+            properties.setProperty(
+                    "docs.file",
+                    "/Users/robertnewson/Downloads/enwiki-20190120-pages-articles-multistream.xml");
+            Config config = new Config(properties);
+
+            final ContentSource contentSource = new EnwikiContentSource();
+            contentSource.setConfig(config);
+
+            docMaker = new DocMaker();
+            docMaker.setConfig(config, contentSource);
+            docMaker.resetInputs();
         }
 
         @TearDown(Level.Iteration)
